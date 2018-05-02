247efd31152b20d812f40b1de7b40a7ee8d60402
Harry Gray <harry@Harrys-MacBook-Pro-2.local>
2018-05-02 16:13:18 +0100

    fixed a big with getTargetSet

--- src/getTargetSet.cpp
+++ src/getTargetSet.cpp
@@ -8,31 +8,36 @@ arma::cube getTargetSet(arma::mat X) {
   // useful quantities
   const float n = X.n_cols;
   const float p = X.n_rows;
+  arma::vec sds1(p);
+  sds1.ones(p);
   arma::vec vars = arma::diagvec(X * X.t()) / n;
-  const float vbar = sum(vars) / p;
+  arma::vec sds2(p);
+  sds2.fill(sqrt(sum(vars) / p));
+  arma::vec sds3 = sqrt(vars);
+  
+  arma::mat cormat1 = arma::eye(p, p);
   const float rbar = (arma::accu(arma::cor(X.t(), 1))-p) / (p * (p + 1));
+  arma::mat cormat2 = rbar * arma::ones(p, p) - rbar * arma::eye(p, p) + arma::eye(p, p);
+  arma::vec rbarpow(p);
+  for (int j = 0; j < p; j++){
+    rbarpow(j) = pow(rbar, j);
+  }
+  arma::mat cormat3 = arma::toeplitz(rbarpow, rbarpow);
+
   int ntargs = 9;
   arma::cube targets(p, p, ntargs);
   //std::cout << "After quantities! Before targets!" << "\n";
 
   // create targets
   targets.slice(0) = arma::eye(p, p);
-  targets.slice(1) = vbar * arma::eye(p, p);
-  targets.slice(2) = arma::diagmat(vars);
-  targets.slice(3) = rbar * arma::ones(p, p) - rbar * arma::eye(p, p) + arma::eye(p, p);
-  targets.slice(4) = targets.slice(3) - arma::eye(p, p) + targets.slice(1);
-  targets.slice(5) = targets.slice(3) - arma::eye(p, p) + targets.slice(2);
-  arma::vec rbarpow(p);
-  
-  //std::cout << "After targets! Before rbarpow!" << "\n";
-  
-  for (int j = 0; j < p; j++){
-    rbarpow(j) = pow(rbar, j);
-  }
-  arma::mat cormat = arma::toeplitz(rbarpow, rbarpow);
-  targets.slice(6) = cormat;
-  targets.slice(7) = cormat - arma::eye(p, p) + targets.slice(1);
-  targets.slice(8) = cormat - arma::eye(p, p) + targets.slice(2);
+  targets.slice(1) = arma::diagmat(sds2) * cormat1 * arma::diagmat(sds2);
+  targets.slice(2) = arma::diagmat(sds3) * cormat1 * arma::diagmat(sds3);
+  targets.slice(3) = arma::diagmat(sds1) * cormat2 * arma::diagmat(sds1);
+  targets.slice(4) = arma::diagmat(sds2) * cormat2 * arma::diagmat(sds2);
+  targets.slice(5) = arma::diagmat(sds3) * cormat2 * arma::diagmat(sds3);
+  targets.slice(6) = arma::diagmat(sds1) * cormat3 * arma::diagmat(sds1);
+  targets.slice(7) = arma::diagmat(sds2) * cormat3 * arma::diagmat(sds2);
+  targets.slice(8) = arma::diagmat(sds3) * cormat3 * arma::diagmat(sds3);
   
   return targets;
 }
